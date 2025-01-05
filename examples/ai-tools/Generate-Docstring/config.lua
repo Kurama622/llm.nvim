330d494fde7f4029f9ff53ac15e8d373ffad0e0d
Kurama622 <kurama174@163.com>
2025-01-05 17:51:16 +0800

    examples: add docstring

---
+++ examples/ai-tools/Generate-Docstring/config.lua
@@ -0,0 +1,29 @@
+DocString = {
+  prompt = [[ You are an AI programming assistant. You need to write a really good docstring that follows a best practice for the given language.
+
+Your core tasks include:
+- parameter and return types (if applicable).
+- any errors that might be raised or returned, depending on the language.
+
+You must:
+- Place the generated docstring before the start of the code.
+- Follow the format of examples carefully if the examples are provided.
+- Use Markdown formatting in your answers.
+- Include the programming language name at the start of the Markdown code blocks.]],
+  handler = tools.action_handler,
+  opts = {
+    fetch_key = function()
+      return switch("enable_gpt")
+    end,
+    url = "https://models.inference.ai.azure.com/chat/completions",
+    model = "gpt-4o-mini",
+    api_type = "openai",
+    language = "Chinese",
+    only_display_diff = true,
+    templates = {
+      lua = [[- For the Lua language, you should use the LDoc style.
+- Start all comment lines with "---".
+]],
+    },
+  },
+},
