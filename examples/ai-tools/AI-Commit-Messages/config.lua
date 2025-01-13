0c93295fe1ffea74cce4fc035da0aa6f47847df5
Kurama622 <kurama174@163.com>
2025-01-13 18:41:12 +0800

    examples: added lazygit integration for AI-Commit-Messages

--- examples/ai-tools/AI-Commit-Messages/config.lua
+++ examples/ai-tools/AI-Commit-Messages/config.lua
@@ -37,6 +37,11 @@ Based on this format, generate appropriate commit messages. Respond with message
       action = function()
         local contents = vim.api.nvim_buf_get_lines(0, 0, -1, true)
         vim.api.nvim_command(string.format('!git commit -m "%s"', table.concat(contents)))
+
+        -- just for lazygit
+        vim.schedule(function()
+          vim.api.nvim_command("LazyGit")
+        end)
       end,
     },
   },
