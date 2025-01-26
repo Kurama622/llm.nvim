dc5ce90623babbc20439df61a86bb296c0ded986
Kurama622 <kurama174@163.com>
2025-01-26 22:18:36 +0800

    examples: update code completion example, add deepseek config

--- examples/ai-tools/Code-Completions/config.lua
+++ examples/ai-tools/Code-Completions/config.lua
@@ -1,9 +1,25 @@
 Completion = {
   handler = tools.completion_handler,
   opts = {
+    --------------------------------
+    ---         ollama
+    --------------------------------
     url = "http://localhost:11434/v1/completions",
     model = "qwen2.5-coder:1.5b",
     api_type = "ollama",
+    ---------- end ollama ----------
+
+    --------------------------------
+    ---         deepseek
+    --------------------------------
+    -- url = "https://api.deepseek.com/beta/completions",
+    -- model = "deepseek-chat",
+    -- api_type = "deepseek",
+    -- fetch_key = function()
+    --   return "your api key"
+    -- end,
+    --------- end deepseek ---------
+
     n_completions = 3,
     context_window = 512,
     max_tokens = 256,
