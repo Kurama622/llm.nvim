6d002debc2012ae13c39033428617a2bf221c59d
Kurama622 <kurama174@163.com>
2025-01-29 23:49:08 +0800

    update: completion (virtual text) add debounce

--- lua/llm/common/completion/frontends/virtual_text.lua
+++ lua/llm/common/completion/frontends/virtual_text.lua
@@ -1,14 +1,76 @@
 -- referenced from minuet-ai.nvim: https://github.com/milanglacier/minuet-ai.nvim
 local state = require("llm.state")
 local LOG = require("llm.common.log")
+local utils = require("llm.common.completion.utils")
+local uv = vim.uv or vim.loop
 
 local virtual_text = {
   rendered = false,
   choice = nil,
   ns_id = vim.api.nvim_create_namespace("llm.virtualtext"),
   extmark_id = 1,
+  timer = nil,
+  is_on_throttle = false,
+  timestamp = 0,
 }
 
+function virtual_text:start()
+  if self.is_on_throttle then
+    return
+  end
+
+  self:stop_timer()
+
+  local bufnr = vim.api.nvim_get_current_buf()
+
+  self.timer = vim.defer_fn(function()
+    if self.is_on_throttle then
+      return
+    end
+
+    self.is_on_throttle = true
+    vim.defer_fn(function()
+      self.is_on_throttle = false
+    end, self.opts.throttle)
+
+    self:trigger(bufnr)
+  end, self.opts.debounce)
+end
+
+function virtual_text:trigger(bufnr)
+  if bufnr ~= vim.api.nvim_get_current_buf() or vim.fn.mode() ~= "i" then
+    return
+  end
+  local timestamp = uv.now()
+  self.timestamp = timestamp
+  local context = utils.get_context(utils.make_cmp_context(), self.opts)
+  local language = utils.add_language_comment()
+  local tab = utils.add_tab_comment()
+  if self.opts.fim then
+    self.opts.prompt = language .. "\n" .. tab .. "\n" .. context.lines_before
+    self.opts.suffix = context.lines_after
+  end
+
+  self:clear()
+  self.opts.exit_handler = function()
+    if timestamp ~= self.timestamp then
+      return
+    end
+    self:preview()
+  end
+
+  if self.opts.prompt or self.opts.suffix then
+    state.completion.backend.request(self.opts)
+  end
+end
+
+function virtual_text:stop_timer()
+  if self.timer and not self.timer:is_closing() then
+    self.timer:stop()
+    self.timer:close()
+    self.timer = nil
+  end
+end
 function virtual_text:clear()
   if self.rendered then
     self.rendered = false
@@ -86,5 +148,96 @@ function virtual_text:prev()
     self.choice = (self.choice - 1) % #state.completion.contents
     self:update_preview()
   end
+end
+
+function virtual_text:autocmd()
+  if state.completion.set_autocmd then
+    return
+  end
+  local virt_group = vim.api.nvim_create_augroup("virt_completions", { clear = true })
+
+  vim.api.nvim_create_autocmd("InsertLeavePre", {
+    group = virt_group,
+    callback = function()
+      if not state.completion.enable then
+        return
+      end
+      utils.terminate_all_jobs()
+      self:clear()
+    end,
+  })
+  if self.opts.auto_trigger then
+    vim.api.nvim_create_autocmd("InsertEnter", {
+      group = virt_group,
+      callback = function()
+        if not state.completion.enable then
+          return
+        end
+        self:start()
+      end,
+    })
+    vim.api.nvim_create_autocmd("TextChangedI", {
+      group = virt_group,
+      callback = function()
+        if not state.completion.enable then
+          return
+        end
+        self:start()
+      end,
+    })
+  end
+  vim.api.nvim_create_autocmd("CompleteDone", {
+    group = virt_group,
+    callback = function()
+      if not state.completion.enable then
+        return
+      end
+      utils.terminate_all_jobs()
+      self:clear()
+    end,
+  })
+  state.completion.set_autocmd = true
+end
+
+function virtual_text:keymap()
+  if state.completion.set_keymap then
+    return
+  end
+  local callbacks = {
+    accept = function()
+      if not state.completion.enable then
+        LOG:WARN("Please enable llm.nvim completion.")
+        return
+      end
+      if self.rendered then
+        self:accept()
+      elseif not self.opts.auto_trigger then
+        LOG:TRACE("Manual trigger completion.")
+        self:start()
+      end
+    end,
+    next = function()
+      self:next()
+    end,
+    prev = function()
+      self:prev()
+    end,
+    toggle = function()
+      state.completion.enable = not state.completion.enable
+      if state.completion.enable then
+        LOG:INFO("Enable llm.nvim completion.")
+      else
+        LOG:INFO("Disable llm.nvim completion.")
+      end
+    end,
+  }
+  for name, km in pairs(self.opts.keymap.virtual_text) do
+    vim.api.nvim_set_keymap(km.mode, km.keys, "", {
+      callback = callbacks[name],
+      noremap = true,
+      silent = true,
+    })
+  end
+  state.completion.set_keymap = true
 end
 return virtual_text
--- lua/llm/common/completion/init.lua
+++ lua/llm/common/completion/init.lua
@@ -1,7 +1,3 @@
-local LOG = require("llm.common.log")
-local state = require("llm.state")
-local utils = require("llm.common.completion.utils")
-
 local completion = {}
 
 function completion:init(opts)
@@ -9,118 +5,9 @@ function completion:init(opts)
   self.backend = require("llm.common.completion.backends")(opts)
   self.frontend = require("llm.common.completion.frontends")(opts)
   if opts.style == "virtual_text" then
-    self:autocmd()
-    self:keymap()
+    self.frontend:autocmd()
+    self.frontend:keymap()
   end
 end
 
-function completion:autocmd()
-  if state.completion.set_autocmd then
-    return
-  end
-  local completion_group = vim.api.nvim_create_augroup("completion", { clear = true })
-
-  vim.api.nvim_create_autocmd("InsertLeavePre", {
-    group = completion_group,
-    callback = function()
-      if not state.completion.enable then
-        return
-      end
-      utils.terminate_all_jobs()
-      self.frontend:clear()
-    end,
-  })
-  if self.opts.auto_trigger then
-    vim.api.nvim_create_autocmd("InsertEnter", {
-      group = completion_group,
-      callback = function()
-        if not state.completion.enable then
-          return
-        end
-        self:start()
-      end,
-    })
-    vim.api.nvim_create_autocmd("TextChangedI", {
-      group = completion_group,
-      callback = function()
-        if not state.completion.enable then
-          return
-        end
-        self:start()
-      end,
-    })
-  end
-  vim.api.nvim_create_autocmd("CompleteDone", {
-    group = completion_group,
-    callback = function()
-      if not state.completion.enable then
-        return
-      end
-      utils.terminate_all_jobs()
-      self.frontend:clear()
-    end,
-  })
-  state.completion.set_autocmd = true
-end
-
-function completion:keymap()
-  if state.completion.set_keymap then
-    return
-  end
-  local callbacks = {
-    accept = function()
-      if not state.completion.enable then
-        LOG:WARN("Please enable llm.nvim completion.")
-        return
-      end
-      if self.frontend.rendered then
-        self.frontend:accept()
-      elseif not self.opts.auto_trigger then
-        LOG:TRACE("Manual trigger completion.")
-        self:start()
-      end
-    end,
-    next = function()
-      self.frontend:next()
-    end,
-    prev = function()
-      self.frontend:prev()
-    end,
-    toggle = function()
-      state.completion.enable = not state.completion.enable
-      if state.completion.enable then
-        LOG:INFO("Enable llm.nvim completion.")
-      else
-        LOG:INFO("Disable llm.nvim completion.")
-      end
-    end,
-  }
-  for name, km in pairs(self.opts.keymap.virtual_text) do
-    vim.api.nvim_set_keymap(km.mode, km.keys, "", {
-      callback = callbacks[name],
-      noremap = true,
-      silent = true,
-    })
-  end
-  state.completion.set_keymap = true
-end
-
-function completion:start()
-  local context = utils.get_context(utils.make_cmp_context(), self.opts)
-  local language = utils.add_language_comment()
-  local tab = utils.add_tab_comment()
-  if self.opts.fim then
-    self.opts.prompt = language .. "\n" .. tab .. "\n" .. context.lines_before
-    self.opts.suffix = context.lines_after
-  end
-
-  self.frontend:clear()
-  self.opts.exit_handler = function()
-    self.frontend:preview()
-  end
-
-  if self.opts.prompt or self.opts.suffix then
-    self.backend.request(self.opts)
-  end
-end
 return completion
