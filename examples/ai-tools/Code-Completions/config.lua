a1893e89e8ad900d405541a3a8a4b43c8c00e2ae
Kurama622 <kurama174@163.com>
2025-02-08 23:06:24 +0800

    example: update code completion, support codeium

--- examples/ai-tools/Code-Completions/config.lua
+++ examples/ai-tools/Code-Completions/config.lua
@@ -1,36 +1,52 @@
 Completion = {
   handler = tools.completion_handler,
   opts = {
-    --------------------------------
-    ---         ollama
-    --------------------------------
+    -------------------------------------------------
+    ---                   ollama
+    -------------------------------------------------
     url = "http://localhost:11434/v1/completions",
     model = "qwen2.5-coder:1.5b",
     api_type = "ollama",
-    ---------- end ollama ----------
+    ------------------- end ollama ------------------
 
-    --------------------------------
-    ---         deepseek
-    --------------------------------
+    -------------------------------------------------
+    ---                  deepseek
+    -------------------------------------------------
     -- url = "https://api.deepseek.com/beta/completions",
     -- model = "deepseek-chat",
     -- api_type = "deepseek",
     -- fetch_key = function()
     --   return "your api key"
     -- end,
-    --------- end deepseek ---------
+    ------------------ end deepseek -----------------
+
+    -------------------------------------------------
+    ---                  codeium
+    -------------------------------------------------
+    -- api_type = "codeium",
+    ------------------ end codeium ------------------
 
     n_completions = 3,
     context_window = 512,
     max_tokens = 256,
+
     -- A mapping of filetype to true or false, to enable completion.
     filetypes = { sh = false },
+
     -- Whether to enable completion of not for filetypes not specifically listed above.
     default_filetype_enabled = true,
+
     auto_trigger = true,
+
+    -- just trigger by { "@", ".", "(", "[", ":", " " } for `style = "nvim-cmp"`
+    only_trigger_by_keywords = true,
+
     style = "virtual_text", -- nvim-cmp or blink.cmp
+
     timeout = 10, -- max request time
-    throttle = 1000, -- only send the request every x milliseconds, use 0 to disable throttle.
+
+    -- only send the request every x milliseconds, use 0 to disable throttle.
+    throttle = 1000,
     -- debounce the request in x milliseconds, set to 0 to disable debounce
     debounce = 400,
 
