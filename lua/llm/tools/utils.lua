3c61066eba3514bab0d693baaa3aadac6e64a1bb
Kurama622 <kurama174@163.com>
2025-04-08 23:18:53 +0800

    feat: enhance suggestion parsing and handling
    - Add parse_suggestion function for extracting code suggestions
    
    - Implement get_hunk_idx to determine current suggestion index
    
    - Update copy_suggestion_code and new_diff to use new parsing logic

--- lua/llm/tools/utils.lua
+++ lua/llm/tools/utils.lua
@@ -2,6 +2,40 @@ local LOG = require("llm.common.log")
 local F = require("llm.common.api")
 local utils = {}
 
+function utils.parse_suggestion(suggestion, pattern)
+  local res = {}
+  local linenr = 0
+  local pos = 1
+  local range_tbl = {}
+  while true do
+    local s, e, c = string.find(suggestion, pattern, pos)
+    if not s then
+      break
+    end
+
+    table.insert(res, c)
+    linenr = linenr + vim.tbl_count(vim.split(string.sub(suggestion, pos, s - 2), "\n"))
+    local start_nr = linenr
+    linenr = linenr + vim.tbl_count(vim.split(string.sub(suggestion, s, e), "\n"))
+    local end_nr = linenr
+    table.insert(range_tbl, { start_nr, end_nr })
+    pos = e + 2
+  end
+  return res, range_tbl
+end
+
+function utils.get_hunk_idx(range_tbl)
+  local cursor_linenr = vim.api.nvim_win_get_cursor(0)[1]
+  local idx = 0
+  for n, range in ipairs(range_tbl) do
+    idx = n
+    if cursor_linenr > range[1] and cursor_linenr < range[2] + 1 then
+      break
+    end
+  end
+  return idx
+end
+
 function utils.set_keymapping(mode, keymaps, callback, bufnr)
   for _, key in pairs(keymaps) do
     vim.api.nvim_buf_set_keymap(bufnr, mode, key, "", { callback = callback })
@@ -32,33 +66,25 @@ end
 
 function utils.copy_suggestion_code(opts, suggestion)
   local pattern = string.format("%s%%w*\n(.-)\n%s", opts.start_str, opts.end_str)
-  local res = {}
-  for match in suggestion:gmatch(pattern) do
-    for _, value in ipairs(vim.split(match, "\n")) do
-      table.insert(res, value)
-    end
-  end
+  local res, range_tbl = utils.parse_suggestion(suggestion, pattern)
   if vim.tbl_isempty(res) then
     LOG:WARN("The code block format is incorrect, please manually copy the generated code.")
   else
-    vim.fn.setreg("+", table.concat(res, "\n"))
+    local idx = utils.get_hunk_idx(range_tbl)
+    vim.fn.setreg("+", res[idx])
   end
 end
 
 function utils.new_diff(diff, opts, context, suggestion)
   local pattern = string.format("%s%%w*\n(.-)\n%s", opts.start_str, opts.end_str)
-  local res = {}
-  for match in suggestion:gmatch(pattern) do
-    for _, value in ipairs(vim.split(match, "\n")) do
-      table.insert(res, value)
-    end
-  end
+  local res, range_tbl = utils.parse_suggestion(suggestion, pattern)
   if vim.tbl_isempty(res) then
     LOG:WARN("The code block format is incorrect, please manually copy the generated code.")
   else
     local contents = vim.api.nvim_buf_get_lines(context.bufnr, 0, -1, true)
+    local idx = utils.get_hunk_idx(range_tbl)
 
-    utils.overwrite_selection(context, res)
+    utils.overwrite_selection(context, vim.split(res[idx], "\n"))
     setmetatable(diff, {
       __index = diff.new({
         bufnr = context.bufnr,
