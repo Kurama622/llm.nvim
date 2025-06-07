011327065eb2070c996f5ba60f25ea464377c152
Kurama622 <kurama174@163.com>
2025-06-07 18:28:33 +0800

    fix: improve JSON argument decoding in OpenAI backend
    - Use format_json_str to preprocess arguments
    
    - Handle edge cases like whitespace and braces

--- lua/llm/backends/utils.lua
+++ lua/llm/backends/utils.lua
@@ -25,4 +25,91 @@ function utils.format_reason(content)
   })
 end
 
+local function trim(str)
+  return str:match("^%s*(.-)%s*$")
+end
+
+local function remove_braces(str)
+  return str:sub(2, #str - 1)
+end
+
+local function remove_colon(str)
+  local i, j = 1, #str
+  while str:sub(i, i) ~= '"' and i < #str do
+    i = i + 1
+  end
+
+  str = str:sub(1, i - 1) .. str:sub(i + 1)
+  while str:sub(j, j) ~= '"' and j > 1 do
+    j = j - 1
+  end
+  return str:sub(1, j - 1) .. str:sub(j + 1)
+end
+
+local function serialization(val)
+  local left_space = 1
+  while val:sub(left_space, left_space) == " " do
+    left_space = left_space + 1
+  end
+
+  local right_space = #val
+  while val:sub(right_space, right_space) == " " do
+    right_space = right_space - 1
+  end
+
+  return val:sub(1, left_space - 1) .. vim.json.encode(val:sub(left_space, right_space)) .. val:sub(right_space + 1)
+end
+
+local function get_item_value(s)
+  local cnt = 0
+  local len = #s
+  local j = len
+
+  while cnt < 2 do
+    local c = s:sub(j, j)
+    if c == '"' then
+      cnt = cnt + 1
+    end
+    if j < 1 then
+      return "", s
+    end
+    j = j - 1
+  end
+
+  while s:sub(j, j) ~= '"' and j > 0 do
+    j = j - 1
+  end
+
+  return serialization(remove_colon(s:sub(1, j))), s:sub(j + 1)
+end
+
+function utils.format_json_str(str)
+  str = remove_braces(trim(str))
+  local list = {}
+  for token in string.gmatch(str, "([^:]+)") do
+    table.insert(list, token)
+  end
+  local res = "{"
+  local num = #list
+  local remainer = ""
+  for i = 1, num do
+    if i == 1 then
+      res = res .. list[i] .. ":"
+    elseif i == num then
+      local s = remove_colon(remainer .. list[i])
+      res = res .. serialization(s)
+      remainer = ""
+    else
+      local value, key = get_item_value(remainer .. list[i])
+      remainer = ""
+      if value ~= "" then
+        res = res .. value .. key .. ":"
+      else
+        remainer = remainer .. key .. ":"
+      end
+    end
+  end
+  res = res .. "}"
+  return res
+end
 return utils
