f84d49a8e3b96e6c76545245bb9e59f47307e9cf
Kurama622 <kurama174@163.com>
2025-06-08 16:54:49 +0800

    feat: add function calling example
    - Create README.md and bash_runner.lua for Function-calling
    
    - Update ai-tools README to include new example link

---
+++ examples/ai-tools/Function-calling/README.md
@@ -0,0 +1,3 @@
+- [BashRunner](./bash_runner.lua)
+
+![BashRunner](https://github.com/user-attachments/assets/75cc90a1-6d54-41bb-9f8f-79b6b710fc4b)
---
+++ examples/ai-tools/Function-calling/bash_runner.lua
@@ -0,0 +1,79 @@
+BashRunner = {
+  handler = tools.qa_handler,
+  prompt = [[Write a suitable bash script and run it through CodeRunner]],
+  opts = {
+    url = "https://api.siliconflow.cn/v1/chat/completions",
+    api_type = "openai",
+    max_tokens = 4096,
+    model = "Qwen/Qwen3-8B",
+    fetch_key = function()
+      return vim.env.SILICONFLOW_TOKEN
+    end,
+    enable_thinking = false,
+
+    component_width = "60%",
+    component_height = "50%",
+    query = {
+      title = "  CodeRunner ",
+      hl = { link = "Define" },
+    },
+    input_box_opts = {
+      size = "15%",
+      win_options = {
+        winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+      },
+    },
+    preview_box_opts = {
+      size = "85%",
+      win_options = {
+        winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+      },
+    },
+    functions_tbl = {
+      CodeRunner = function(code)
+        -- Replace '\\n' that are not enclosed in quotes
+        code = code:gsub("([^\"'])\\n", "%1\n")
+        code = code:gsub("\\n([^\"'])", "\n%1")
+
+        local filepath = "/tmp/script.sh"
+
+        -- Print the code suggested by llm
+        vim.notify(
+          string.format("CodeRunner running...\n```bash\n%s\n```", code),
+          vim.log.levels.INFO,
+          { title = "llm: CodeRunner" }
+        )
+
+        local file = io.open(filepath, "w")
+        if file then
+          file:write(code)
+          file:close()
+          local script_result = vim.system({ "bash", filepath }, { text = true }):wait()
+          os.remove(filepath)
+          return script_result.stdout
+        else
+          return ""
+        end
+      end,
+    },
+    schema = {
+      {
+        type = "function",
+        ["function"] = {
+          name = "CodeRunner",
+          description = "Bash code interpreter",
+          parameters = {
+            properties = {
+              code = {
+                type = "string",
+                description = "bash code",
+              },
+            },
+            required = { "code" },
+            type = "object",
+          },
+        },
+      },
+    },
+  },
+},
