275e32224cb66383ff0c6e3e59045cf3dacf2f72
Kurama622 <kurama174@163.com>
2025-07-05 22:40:45 +0800

    feat: improve git commit command handling
    - Escape '#' in commit messages
    
    - Refactor command construction logic

--- examples/ai-tools/AI-Commit-Messages/config.lua
+++ examples/ai-tools/AI-Commit-Messages/config.lua
@@ -56,8 +56,13 @@ Based on this format, generate appropriate commit messages. Respond with message
       },
       action = function()
         local contents = vim.api.nvim_buf_get_lines(0, 0, -1, true)
-        vim.api.nvim_command(string.format('!git commit -m "%s"', table.concat(contents, '" -m "')))
 
+        local cmd = string.format('!git commit -m "%s"', table.concat(contents, '" -m "'))
+        cmd = (cmd:gsub(".", {
+          ["#"] = "\\#",
+        }))
+
+        vim.api.nvim_command(cmd)
         -- just for lazygit
         vim.schedule(function()
           vim.api.nvim_command("LazyGit")
