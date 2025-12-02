b3cbe7cd2051aab64a31caba279b630414a8672c
Kurama622 <kurama174@163.com>
2025-12-02 21:58:45 +0800

    feat: add language support for C and C++ in LSP
    - Create c.lua for C language specifications
    
    - Extend cpp.lua with additional specifiers

---
+++ lua/llm/lsp/c.lua
@@ -0,0 +1,11 @@
+return {
+  enum_specifier = true,
+  struct_specifier = true,
+  field_declaration = true,
+  function_definition = true,
+  declaration = true,
+  preproc_def = true,
+  preproc_function_def = true,
+  type_definition = true,
+  union_specifier = true,
+}
