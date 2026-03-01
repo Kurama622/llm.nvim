eba5ac582ba4e1843cc54bfc60af5b62fe9eea21
Kurama622 <kurama174@163.com>
2026-03-01 18:54:52 +0800

    Update AI commit msg config

--- examples/ai-tools/AI-Commit-Messages/config.lua
+++ examples/ai-tools/AI-Commit-Messages/config.lua
@@ -57,7 +57,7 @@ Based on this format, generate appropriate commit messages. Respond with message
       action = function()
         local contents = vim.api.nvim_buf_get_lines(0, 0, -1, true)
 
-        local cmd = string.format('!git commit -m "%s"', table.concat(contents, '" -m "'))
+        local cmd = string.format("!git commit -m '%s'", table.concat(contents, "' -m '"))
         cmd = (cmd:gsub(".", {
           ["#"] = "\\#",
           ["%"] = "\\%",
